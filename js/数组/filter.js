75f7fa440ba47c26c2778cf78bee07f9f5c333ba
Study-CCC <1013409555@qq.com>
2019-12-26 23:27:11 +0800

    js学习

--- js/数组/filter.js
+++ js/数组/filter.js
@@ -10,8 +10,8 @@ var arr = ['1','2',undefined, '3.jpg',null,22,""]
 // console.log(arr.filter(item=>item))
 // 3.去掉数组中不符合项
 var arr1 = [20,30,50, 96,50]
-var newArr = arr.filter(item => item>40)  
-// console.log(newArr)
+var newArr = arr1.filter(item => item>40)  
+console.log(newArr)
 // 4.数组去重
 var arr2 = [2,1,4,55,5,5,5,2]
 var result = arr2.filter((a,b,c)=>c.indexOf(a)===b)
---
+++ js/数组/数组空位.js
@@ -0,0 +1,8 @@
+// ES6方法
+var arr = [,,,,,]
+console.log(arr.length)
+console.log(1 in arr)
+arr = [,null,undefined,,,]
+console.log(arr.length)
+console.log(1 in arr)
+console.log(2 in arr)
