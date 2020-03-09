f8061ee02566f6eb5a36a0aeb90ee5caadbd131a
Study-CCC <1013409555@qq.com>
2020-03-09 21:58:47 +0800

    react项目

--- react-study/react_blog/blog/pages/detailed.js
+++ react-study/react_blog/blog/pages/detailed.js
@@ -8,42 +8,41 @@ import Author from '../components/Author'
 import Advert from '../components/Advert'
 import Footer from '../components/Footer'
 import '../public/static/style/pages/detailed.css'
-import ReactMarkdown from 'react-markdown'   //将文章用markdown进行标记
+import marked from 'marked'
+import hljs from "highlight.js";
+import 'highlight.js/styles/monokai-sublime.css';
 const Detailed = () => {
-  let markdown = '# P01:课程介绍和环境搭建\n' +
-    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-    '**这是加粗的文字**\n\n' +
-    '*这是倾斜的文字*`\n\n' +
-    '***这是斜体加粗的文字***\n\n' +
-    '~~这是加删除线的文字~~ \n\n' +
-    '\`console.log(111)\` \n\n' +
-    '# p02:来个Hello World 初始Vue3.0\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n' +
-    '***\n\n\n' +
-    '# p03:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p04:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '#5 p05:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p06:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p07:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '``` var a=11; ```'
+  const renderer = new marked.Renderer();
+  // renderer: 这个是必须填写的，你可以通过自定义的Renderer渲染出自定义的格式
+
+  // gfm：启动类似Github样式的Markdown,填写true或者false
+  
+  // pedatic：只解析符合Markdown定义的，不修正Markdown的错误。填写true或者false
+  
+  // sanitize: 原始输出，忽略HTML标签，这个作为一个开发人员，一定要写flase
+  
+  // tables： 支持Github形式的表格，必须打开gfm选项
+  
+  // breaks: 支持Github换行符，必须打开gfm选项，填写true或者false
+  
+  // smartLists：优化列表输出，这个填写ture之后，你的样式会好看很多，所以建议设置成ture
+  
+  // highlight: 高亮显示规则 ，这里我们将使用highlight.js来完成
+  marked.setOptions({
+    renderer: renderer,    
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+  });
+
+  let html = marked(props.article_content)
   return (
     <>
       <Head>
@@ -102,14 +101,14 @@ const Detailed = () => {
     </>
   )
 }
-Detailed.getInitialProps = async(context)=>{
+Detailed.getInitialProps = async (context) => {
 
   console.log(context.query.id)
-  let id =context.query.id
-  const promise = new Promise((resolve)=>{
+  let id = context.query.id
+  const promise = new Promise((resolve) => {
 
-    axios('http://127.0.0.1:7001/default/getArticleById/'+id).then(
-      (res)=>{
+    axios('http://127.0.0.1:7001/default/getArticleById/' + id).then(
+      (res) => {
         console.log(title)
         resolve(res.data.data[0])
       }
