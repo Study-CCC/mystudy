44629fb255a3b7f328594cf96940f46130a7d32e
Study-CCC <1013409555@qq.com>
2020-08-10 21:52:08 +0800

    回溯

---
+++ 面试准备/leetcode/回溯/93.js
@@ -0,0 +1,23 @@
+const restoreIpAddresses = (s) => { 
+    let res = []
+    const dfs = (strArr,len)=>{
+        console.log(strArr.length,len);
+        if(strArr.length==4&&len==s.length){
+            res.push(strArr.join('.'))
+            return;
+        }else if(strArr.length==4&&len<s.length){
+            return
+        }
+        for(let i=1;i<=3;i++){
+            if(len+i>s.length) return
+            let str = s.substring(len,len+i)
+            if(i!=1&&str[0]=='0') return;
+            if(i==3&&str>255) return;
+            strArr.push(str)
+            dfs(strArr,len+i)
+            strArr.pop()
+        }
+    }
+    dfs([],0)
+    return res
+};
