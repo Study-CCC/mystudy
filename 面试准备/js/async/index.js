de2c99db93f9b8b82a7ff0c73501afffa1f2d399
Study-CCC <1013409555@qq.com>
2020-08-15 16:34:36 +0800

    async

---
+++ 面试准备/js/async/index.js
@@ -0,0 +1,51 @@
+// async 用于处理异步编程
+// 优点：1. 写法简单  2. 语义性强
+// async中reject采用try catch方式捕获
+// async函数返回的是一个promise对象、
+function takeLongTime(n) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(n + 200), n);
+    });
+}
+
+function step1(n) {
+    console.log(`step1 with ${n}`);
+    return takeLongTime(n);
+}
+
+function step2(n) {
+    console.log(`step2 with ${n}`);
+    return takeLongTime(n);
+}
+
+function step3(n) {
+    console.log(`step3 with ${n}`);
+    return takeLongTime(n);
+}
+// 用promise写
+function doIt() {
+    console.time("doIt");
+    const time1 = 300;
+    step1(time1)
+        .then(time2 => step2(time2))
+        .then(time3 => step3(time3))
+        .then(result => {
+            console.log(`result is ${result}`);
+            console.timeEnd("doIt");
+        });
+}
+
+doIt();
+
+// 用async和await写
+async function doIt() {
+    console.time("doIt");
+    const time1 = 300;
+    const time2 = await step1(time1);
+    const time3 = await step2(time2);
+    const result = await step3(time3);
+    console.log(`result is ${result}`);
+    console.timeEnd("doIt");
+}
+
+doIt();
